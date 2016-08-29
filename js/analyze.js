7573930c74ba147c666285c0b66e663041bf301c
Tom Lee <thomas.j.lee@gmail.com>
2016-08-28 23:06:46 -0400

    renaming, plus more analysis

--- js/analyze.js
+++ js/analyze.js
@@ -1,6 +1,11 @@
 var PIXELS_PER_SECOND = 30.0;
 var PIXELS_PER_CM = 2.5;
 var MAX_DIST = 300;
+var COLORS = [[255, 255, 255], [0, 102, 255]];
+
+var queue = require('d3-queue').queue;
+var dist = require('turf-distance');
+var pt = require('turf-point');
 
 function addEvent(el, type, handler) {
     if (el.attachEvent) el.attachEvent('on'+type, handler); else el.addEventListener(type, handler);
@@ -15,15 +20,61 @@ function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
     xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+        if (xhr.readyState>3 && xhr.status==200) success(null, xhr.responseText);
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
     return xhr;
 }
 
-function setup(ajaxResponse) {
-    var data = JSON.parse(ajaxResponse);
+function calculateSpeed(geoData) {
+    var f = geoData.features[0];
+    var out = [];
+    var lastGoodPoint = 0;
+    for(var i = 1; i < f.properties.timestamp.length; i++) {
+        var t = (f.properties.timestamp[i] + f.properties.timestamp[lastGoodPoint]) / 2;
+        var timespan = f.properties.timestamp[i] - f.properties.timestamp[lastGoodPoint];
+        if (timespan < 0.5)
+            continue;
+        var distance = dist(pt(f.geometry.coordinates[i]), pt(f.geometry.coordinates[lastGoodPoint]), 'kilometers');
+        out.push([t - f.properties.timestamp[0], distance / timespan]);
+        lastGoodPoint = i;
+    }
+
+    // convert units from kilometers per sec to mph
+    out = out.map(function(d) {
+        d[1] = d[1] * 0.621371 * 3600;
+        return d;
+    });
+
+    // apply smoothing
+    out = boxcarSmooth(out, 5, 1);
+
+    return out;
+}
+
+function boxcarSmooth(data, width, offset) {
+    for(var i = Math.floor(width / 2); i < data.length - Math.floor(width / 2); i++) {
+        var sum = 0;
+        var total = 0;
+        for(var j = -1 * Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
+            if (data[i + j][offset] !== null) {
+                sum += data[i + j][offset];
+                total++;
+            }
+        }
+
+        if (data[i][offset] !== null)
+            data[i][offset] = sum / total;
+    }
+    return data;
+}
+
+function setup(distanceData, geoData) {
+    var data = JSON.parse(distanceData);
+    var geo = JSON.parse(geoData);
+
+    var speed = calculateSpeed(geo);
 
     MAX_DIST = data.reduce(function(prev, cur) { return Math.max(prev, Math.max(cur[1], cur[2])); }, 0);
 
@@ -35,10 +86,10 @@ function setup(ajaxResponse) {
 
     Array.prototype.slice.call(document.getElementsByTagName('input'), 0).concat(Array.prototype.slice.call(document.getElementsByTagName('select'), 0))
         .forEach(function(el) {
-            addEvent(el, 'change', draw.bind(null, data));
+            addEvent(el, 'change', draw.bind(null, data, speed));
         });
 
-    draw(data);
+    draw(data, speed);
 }
 
 function dist2y(dist) {
@@ -49,7 +100,7 @@ function isChecked(n) {
     return document.getElementById(n) && document.getElementById(n).checked;
 }
 
-function draw(data) {
+function draw(data, speed) {
 
     // set initial t = 0
     var base = data[0][0];
@@ -116,24 +167,9 @@ function draw(data) {
     // boxcar smoothing
     if (isChecked('boxcar')) {
         var BOXCAR_WIDTH = document.getElementById('boxcarWidth').options[document.getElementById('boxcarWidth').selectedIndex].value;
-        console.log(BOXCAR_WIDTH);
-        for(var i = Math.floor(BOXCAR_WIDTH / 2); i < data.length - Math.floor(BOXCAR_WIDTH / 2); i++) {
-            for (var k = 1; k <= 2; k++) {
-                var sum = 0;
-                var total = 0;
-                for(var j = -1 * Math.floor(BOXCAR_WIDTH / 2); j <= Math.floor(BOXCAR_WIDTH / 2); j++) {
-                    if (data[i + j][k] !== null) {
-                        sum += data[i + j][k];
-                        total++;
-                    }
-                }
-
-                if (data[i][k] !== null)
-                    data[i][k] = sum / total;
-            }
-        }
+        data = boxcarSmooth(data, BOXCAR_WIDTH, 1);
+        data = boxcarSmooth(data, BOXCAR_WIDTH, 2);
     }
-
 
     // todo:
     // - highlight correlated sections
@@ -146,6 +182,68 @@ function draw(data) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    // draw speed
+    if (isChecked('speed')) {
+        var maxSpeed = 40; //speed.reduce(function(prev, cur) { return Math.max(prev, cur[1]) }, 0);
+        ctx.fillStyle = '#222';
+        ctx.beginPath();
+        ctx.moveTo(0, canvas.height - (canvas.height * speed[0][1] / maxSpeed));
+        speed.forEach(function(s) {
+            ctx.lineTo(s[0] * PIXELS_PER_SECOND, canvas.height - (canvas.height * s[1] / maxSpeed));
+        });
+        ctx.lineTo(canvas.width, canvas.height - (canvas.height * speed[speed.length - 1][1] / maxSpeed));
+        ctx.lineTo(canvas.width, canvas.height);
+        ctx.lineTo(0, canvas.height);
+        ctx.fill();
+    }
+
+    // derivatives
+    var derivative = data.reduce(function(prev, cur) { prev.push([cur[0], 0, 0]); return prev; }, []);
+    for(var sensor_i = 0; sensor_i < COLORS.length; sensor_i++) {
+        for(var i = 1; i < data.length; i++) {
+            derivative[i][sensor_i + 1] = data[i][sensor_i + 1] - data[i - 1][sensor_i + 1];
+        }
+    }
+
+    var derivative2 = data.reduce(function(prev, cur) { prev.push([cur[0], 0, 0]); return prev; }, []);
+    for(var sensor_i = 0; sensor_i < COLORS.length; sensor_i++) {
+        for(var i = 1; i < data.length; i++) {
+            derivative2[i][sensor_i + 1] = derivative[i][sensor_i + 1] - derivative[i - 1][sensor_i + 1];
+        }
+    }
+
+    if (isChecked('dy')) {
+        COLORS.forEach(function(sensorColor, sensor_i) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'rgba(' + sensorColor.join(',') + ',0.5)';
+            ctx.moveTo(0, canvas.height);
+            for(var i = 1; i < data.length; i++) {
+                ctx.lineTo(data[i][0] * PIXELS_PER_SECOND, dist2y(derivative[i][sensor_i + 1]) - 100);
+            }
+            ctx.stroke();
+        });
+    }
+
+    if (isChecked('dy2')) {
+        COLORS.forEach(function(sensorColor, sensor_i) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'rgba(' + sensorColor.join(',') + ',0.5)';
+            ctx.moveTo(0, canvas.height);
+            for(var i = 1; i < data.length; i++) {
+                ctx.lineTo(data[i][0] * PIXELS_PER_SECOND, dist2y(derivative2[i][sensor_i + 1]) - 100);
+            }
+            ctx.stroke();
+        });
+    }
+
+    if (isChecked('dy') || isChecked('dy2')) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'yellow';
+        ctx.moveTo(0, canvas.height - 100);
+        ctx.lineTo(canvas.width, canvas.height - 100);
+        ctx.stroke();
+    }
+
     // draw passing distance
     ctx.beginPath();
     ctx.strokeStyle = 'red';
@@ -155,11 +253,8 @@ function draw(data) {
     ctx.stroke();
     ctx.setLineDash([10, 1]);
 
-
-
-
-    ['white', '#0066ff'].forEach(function(sensorColor, sensor_i) {
-        ctx.strokeStyle = ctx.fillStyle = sensorColor;
+    COLORS.forEach(function(sensorColor, sensor_i) {
+        ctx.strokeStyle = ctx.fillStyle = 'rgba(' + sensorColor.join(',') + ',1.0)';
 
         var drawing = false;
         for(var pt_i = 0; pt_i < data.length; pt_i++) {
@@ -187,6 +282,13 @@ function draw(data) {
 document.addEventListener('DOMContentLoaded', function() {
     console.log('starting');
 
-    var url = '/data/bd2-20160815-181115-sonar.json';
-    getAjax(url, setup);
+    var url = '/data/bd2-20160815-181115';
+
+    var q = queue();
+    q.defer(getAjax, url + '-sonar.json');
+    q.defer(getAjax, url + '.geojson');
+    q.awaitAll(function(err, results) {
+        if (err) return console.error(err);
+        setup.apply(null, results);
+    });
 });
